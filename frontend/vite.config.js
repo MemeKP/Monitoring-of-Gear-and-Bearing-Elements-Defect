fa0eae22ab5d07d649678fb6b69f5988cecb371c
Panita Donmuang <panipani.d903@gmail.com>
2026-04-28 09:34:06 +0700

    setup vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    // https://stackoverflow.com/a/74430384
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 })
